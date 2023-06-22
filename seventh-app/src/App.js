da79ca23a4d3d9b5fe6e48e80091411607c78943
hegde421201 <hegde421201@gmail.com>
2023-06-22 17:30:20 +0530

    Update App.js
    update

--- seventh-app/src/App.js
+++ seventh-app/src/App.js
@@ -1,5 +1,23 @@
-import "./App.css";
+import React, { useState } from "react";
 
-function App() {}
+const ControlledInputs = () => {
+  return (
+    <>
+      <article>
+        <form className="form">
+          <div className="form-control">
+            <label htmlFor="firstName">Name :</label>
+            <input type="text" id="firstName" name="firstName" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Email :</label>
+            <input type="text" id="email" name="email" />
+          </div>
+          <button type="submit">Add employee</button>
+        </form>
+      </article>
+    </>
+  );
+};
 
-export default App;
+export default ControlledInputs;
